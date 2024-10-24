370709ac25b24dc61f426644440a7e2bbaff8941
lkarine <141235835+lkarine@users.noreply.github.com>
2024-10-24 11:51:30 -0300

    kk

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Programação'
+    'O que é HTML?'
+    'É uma linguagem de marcação de Hipertexto'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Programação'
+    'O que é HTML?'
+    'É uma linguagem de marcação de Hipertexto'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Programação'
+    'O que é HTML?'
+    'É uma linguagem de marcação de Hipertexto'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Programação'
+    'O que é HTML?'
+    'É uma linguagem de marcação de Hipertexto'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Programação'
+    'O que é HTML?'
+    'É uma linguagem de marcação de Hipertexto'
 )
 
 criaCartao(
-    'Programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de Hipertexto',
+    'Programação'
+    'O que é HTML?'
+    'É uma linguagem de marcação de Hipertexto'
 )
